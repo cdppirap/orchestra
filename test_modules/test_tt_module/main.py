7ea770ce4acaa7ba74e7916a0be75f0ecf9c2f90
Alexandre Schulz <alexandre.schulz@irap.omp.eu>
2022-03-03 11:55:10 +0100

    added test_tt_module for testing timetable modules

---
+++ test_modules/test_tt_module/main.py
@@ -0,0 +1,22 @@
+import sys
+import os
+from datetime import datetime, timedelta
+
+td = timedelta(hours=1)
+
+datefmt = "%Y-%m-%dT%H:%M:%S"
+
+output_dir = sys.argv[1]
+parameter = sys.argv[2]
+start = datetime.strptime(sys.argv[3], datefmt)
+stop = datetime.strptime(sys.argv[4], datefmt)
+
+target_filename = os.path.join(output_dir, "out.tt")
+curr_dt = start
+with open(target_filename, "w") as f:
+    while curr_dt < stop:
+        line = "{} {}\n".format(curr_dt.strftime(datefmt), (curr_dt+td).strftime(datefmt))
+        f.write(line)
+        curr_dt += 2*td
+
+
