96cba676db6649d809bf4d1f6622b64b43cd5434
Alexandre Schulz <alexandre.schulz@irap.omp.eu>
2022-03-03 11:47:39 +0100

    corrections to test_cat_module

--- test_modules/test_cat_module/main.py
+++ test_modules/test_cat_module/main.py
@@ -15,7 +15,7 @@ target_filename = os.path.join(output_dir, "out.cat")
 curr_dt = start
 with open(target_filename, "w") as f:
     while curr_dt < stop:
-        line = "{} {} 1\n".format(curr_dt.strftime(datefmt), curr_dt.strftime(datefmt))
+        line = "{} {} 1\n".format(curr_dt.strftime(datefmt), (curr_dt+td).strftime(datefmt))
         f.write(line)
         curr_dt += 2*td
 
