1a9da2adf8552c04f7d5cd82b0cb69cb1f7cf796
Alexandre Schulz <alexandre.schulz@irap.omp.eu>
2022-03-08 10:31:30 +0100

    correction to command line argument management

--- orchestra/__main__.py
+++ orchestra/__main__.py
@@ -26,6 +26,8 @@ if __name__=="__main__":
         os.system("rm -rf {}/*".format(config.task_directory))
         mod_manager.clear()
     # register a new model
+    if args.register is None:
+        args.register = []
     for od in args.register:
         if od is not None:
             if os.path.exists(od):
