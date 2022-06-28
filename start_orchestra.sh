e9ca845808f9a188f3f57a333d1d815a9faf0b64
Alexandre Schulz <alexandre.schulz@irap.omp.eu>
2022-06-28 11:03:26 +0200

    startup script for orchestra

--- start_orchestra.sh
+++ start_orchestra.sh
@@ -1,11 +1,7 @@
-# move into the installation directory
-cd /home/aschulz/Documents/python/orchestra
-
 # source environment variables
 . orchestra_env.sh
 
 # activate the virtual environment
-. venv/bin/activate
 echo "[$(date --iso-8601=seconds)] Starting orchestra REST service" >> orchestra.log
 #python -m orchestra.rest &
 flask init-db
