d72e0dce5d775f150cb9d1cf311b875ddea9c8df
Alexandre Schulz <alexandre.schulz@irap.omp.eu>
2022-06-28 12:15:04 +0200

    user model password field length

--- orchestra/webservice/auth/models.py
+++ orchestra/webservice/auth/models.py
@@ -6,7 +6,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(64), nullable=False)
+    password = db.Column(db.String(1024), nullable=False)
 
     @property
     def is_authenticated(self):
