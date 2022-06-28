8b374ba7fbe5f5451a0a80aa50faf50a42b0f635
Alexandre Schulz <alexandre.schulz@irap.omp.eu>
2022-06-28 15:08:12 +0200

    start celery with flask

--- start_orchestra.sh
+++ start_orchestra.sh
@@ -5,6 +5,10 @@
 echo "[$(date --iso-8601=seconds)] Starting orchestra REST service" >> orchestra.log
 #python -m orchestra.rest &
 
+# celery
+python -m celery -A orchestra.webservice.app.celery worker --loglevel=info
+
+# webserver
 python -m flask init-db
 python -m flask run --host 0.0.0.0
 #flask init-db
