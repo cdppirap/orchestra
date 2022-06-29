8aac358a3a2d9644aa1f35199aeb85b5e904a700
Alexandre Schulz <alexandre.schulz@irap.omp.eu>
2022-06-29 15:25:29 +0200

    added ORCHESTRA_HOSTNAME for dashboard link

--- orchestra_env.sh
+++ orchestra_env.sh
@@ -1,5 +1,6 @@
 # Project
 export ORCHESTRA_BRANCH="with_flower" # admin_website
+export ORCHESTRA_HOSTNAME=$HOSTNAME
 
 # Orchestra user
 export ORCHESTRA_USER=$USER
@@ -10,13 +11,15 @@ export ORCHESTRA_GID=$(id -g $USER)
 export DOCKER_GID=$(getent group docker | cut -d: -f3)
 
 # Postgresql database
-export POSTGRESQL_DB=orchestra
-export POSTGRESQL_USER=orchestra
-export POSTGRESQL_PASSWORD=orchestra
+export POSTGRESQL_DB=test_database #orchestra
+export POSTGRESQL_USER=$USER #orchestra
+export POSTGRESQL_PASSWORD= #orchestra
 export POSTGRESQL_HOSTNAME=localhost
+export POSTGRESQL_PORT=5432
 
 # Redis server
 export REDIS_HOSTNAME=localhost
+export REDIS_PORT=6379
 
 # Flask 
 export FLASK_APP=orchestra.webservice
@@ -25,20 +28,4 @@ export FLASK_DEBUG=0
 export FLASK_HOST=0.0.0.0
 export FLASK_PORT=5000
 
-# For debug
-#echo "ORCHESTRA_USER: $ORCHESTRA_USER"
-#echo "ORCHESTRA_UID: $ORCHESTRA_UID"
-#echo "ORCHESTRA_GID: $ORCHESTRA_GID"
-#echo ""
-#echo "DOCKER_GID: $DOCKER_GID"
-#echo ""
-#echo "POSTGRESQL_USER: $POSTGRESQL_USER" 
-#echo "POSTGRESQL_PASSWORD: $POSTGRESQL_PASSWORD"
-#echo "POSTGRESQL_HOSTNAME: $POSTGRESQL_HOSTNAME"
-#echo "POSTGRESQL_DB: $POSTGRESQL_DB"
-#echo ""
-#echo "REDIS_HOSTNAME: $REDIS_HOSTNAME"
-#echo ""
-#echo "FLASK_APP: $FLASK_APP"
-#echo "FLASK_ENV: $FLASK_ENV"
-#echo "FLASK_DEBUG: $FLASK_DEBUG"
+
